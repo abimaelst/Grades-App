4ba3b95d3c26e533e5c037ebbbcb270318342f37
abimaelst <abimaelst@live.com>
2020-07-08 14:13:50 -0300

    config method, cors and  deploy herokuu

---
+++ build/precache-manifest.81a7b1e1c7cfadf68fcc13bd07037eca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3facadaa40acea043002afbf96d51164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9859868f1fe7be85f8d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a99c3ad28f766b5ce808",
+    "url": "/static/css/main.b5fec8d3.chunk.css"
+  },
+  {
+    "revision": "f9859868f1fe7be85f8d",
+    "url": "/static/js/2.e881570d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e881570d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99c3ad28f766b5ce808",
+    "url": "/static/js/main.b38a4542.chunk.js"
+  },
+  {
+    "revision": "0e1d41acf44fb6fb584c",
+    "url": "/static/js/runtime-main.1fee81f7.js"
+  }
+]);
